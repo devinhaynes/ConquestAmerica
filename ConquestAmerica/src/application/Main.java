c5b166d7becd88d126a6ac0d9850728f895fbfd4
DrakeStrange <47987208+DrakeStrange@users.noreply.github.com>
2019-03-27 09:20:12 -0400

    Add files via upload

--- ConquestAmerica/src/application/Main.java
+++ ConquestAmerica/src/application/Main.java
@@ -14,7 +14,7 @@ public class Main extends Application {
 		try {
 			Parent root = FXMLLoader.load(getClass().getResource("Start.fxml"));
 			Scene scene = new Scene(root);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			
 			primaryStage.setScene(scene);
 			primaryStage.setTitle("Conquest America");
 			primaryStage.show();
